hopefully fixed evpa rotation in movie()

vcat/image_cube.py
@@ -1424,12 +1424,12 @@ class ImageCube(object):
                 if i>0:
                     for k in range(len(evpa)):
                         for j in range(len(evpa[0])):
-                            if evpa[k][j]-evpas[i-1][k][j]>90:
+                            if evpa[k][j]-evpas[i-1][k][j]>np.pi/2:
                                 for ind in range(i,len(evpas)):
-                                    evpas[ind][k][j]-=180
-                            if evpa[k][j]-evpas[i-1][k][j]<-90:
+                                    evpas[ind][k][j]-=np.pi
+                            if evpa[k][j]-evpas[i-1][k][j]<-np.pi/2:
                                 for ind in range(i,len(evpas)):
-                                    evpas[ind][k][j]+=180
+                                    evpas[ind][k][j]+=np.pi
 
             #EVPA
             interp_evpa = RegularGridInterpolator(grid, evpas, method='linear', bounds_error=False,