e91f6167fd8eb9bdaf6ce9082625ab7c5d3f99f7
Florian <florian.eppel@7fun.de>
2025-07-10 16:07:13 +0200

    minor bug fix

--- vcat/__init__.py
+++ vcat/__init__.py
@@ -2,7 +2,7 @@
 from vcat.image_data import ImageData
 from vcat.plots.fits_image import FitsImage
 from vcat.plots.multi_fits_image import MultiFitsImage
-from vcat.plot.model_image_plot import ModelImagePlot
+from vcat.plots.model_image_plot import ModelImagePlot
 from vcat.plots.kinematic_plot import KinematicPlot  #probably not needed
 from vcat.plots.evolution_plot import EvolutionPlot
 from vcat.image_cube import ImageCube
